4136fd0266eb080ea3228292ddeb63558209b553
Larionov-Semyon <37975787+Larionov-Semyon@users.noreply.github.com>
2021-10-17 17:41:23 +0300

    17.10.21 - test, readme

--- main.py
+++ main.py
@@ -1,94 +1,101 @@
+"""
+	Игра крестики-нолики
+"""
+
 import os
 from combinations import combinations
 
 
 class TicTacGame:
-	n = 3
+    """Класс игры крестики-нолики"""
+    n = 3
 
-	def __init__(self):
-		self.player1 = self.input_name('Введите имя 1-ого игрока: ')
-		self.player2 = self.input_name('Введите имя 2-ого игрока: ')
-		self.players = {0: (self.player1, 'o'), 1: (self.player2, '+')}
-		self.board = {i: i for i in range(1, self.n**2 + 1)}
+    def __init__(self):
+        self.count = 3
+        self.players = {0: ['player1', 'o'], 1: ['player2', '+']}
+        self.board = {i: i for i in range(1, self.count ** 2 + 1)}
 
-	def input_name(self, text):
-		"""Проверка на ввод имени игрока
-			Вывод: валидное имя игрока"""
-		name = input(text)
-		if name.isalpha():
-			return name
-		else:
-			print("Извините, можно использовать только буквы. Повторите ввод имени: ")
-			return self.input_name(text)
+    def input_name(self, text):
+        """Проверка на ввод имени игрока
+            Вывод: валидное имя игрока"""
+        name = input(text)
+        if name.isalpha():
+            return name
+        print("Извините, можно использовать только буквы. Повторите ввод имени: ")
+        return self.input_name(text)
 
-	def start_game(self):
-		"""Игра"""
-		current_ind = 0
-		next_ind = 1
-		while self.check_winner() is None:
-			os.system('cls')
-			print("Ход игрока - {}".format(self.players[current_ind][0]))
-			self.show_board()
-			self.step(current_ind)
-			current_ind, next_ind = next_ind, current_ind
+    def start_game(self):
+        """Старт игры"""
+        for i, player in self.players.items():
+            text = f'Введите имя {i + 1}-ого игрока: '
+            player[0] = self.input_name(text)
 
-		os.system('cls')
-		self.show_board()
-		print("WINNER - {}  !!!".format(self.check_winner()))
+        current_ind = 0
+        next_ind = 1
+        while self.check_winner() is None:
+            os.system('cls')
+            print(f'Ход игрока - {self.players[current_ind][0]}')
+            self.show_board()
+            self.step(current_ind)
+            current_ind, next_ind = next_ind, current_ind
 
-	def step(self, ind):
-		"""Ход игрока"""
-		x = input("Введите координату: ")
-		if self.validate_input(x):
-			self.board[int(x)] = self.players[ind][1]
-		else:
-			self.step(ind)
+        os.system('cls')
+        self.show_board()
+        print(f'WINNER - {self.check_winner()}  !!!')
 
-	def show_board(self):
-		"""Вывод игрового поля"""
-		print("Рабочее поле:")
-		print('\t' + '---' * self.n)
-		string = []
-		for k, v in self.board.items():
-			string.append(v)
-			if k % 3 == 0:
-				print('\t' + ' | '.join(map(str, string)))
-				print('\t' + '---' * self.n)
-				string = []
+    def step(self, ind):
+        """Ход игрока"""
+        num = input("Введите координату: ")
+        if self.validate_input(num):
+            self.board[int(num)] = self.players[ind][1]
+        else:
+            self.step(ind)
 
-	def validate_input(self, x):
-		"""Проверка ввода координаты
-			Вывод: True - если знач. валидно"""
-		if not x.isdigit():
-			print('Вы ввели не число')
-			return False
-		elif not 1 <= int(x) <= self.n**2:
-			print('Число превысила допустимые пределы')
-			return False
-		elif not isinstance(self.board[int(x)], int):
-			print('Это поле уже занято')
-			return False
-		else:
-			return True
+    def show_board(self):
+        """Вывод игрового поля"""
+        print("Рабочее поле:")
+        print('\t' + '---' * self.count)
+        string = []
+        for key, value in self.board.items():
+            string.append(value)
+            if key % 3 == 0:
+                print('\t' + ' | '.join(map(str, string)))
+                print('\t' + '---' * self.count)
+                string = []
 
-	def check_winner(self):
-		"""Проверка на присутствие победителя или ничьи"""
-		# проверка выигрывающих комбинаций
-		for name, sign in self.players.values():
-			for x, y, z in combinations:
-				if self.board[x] == self.board[y] == self.board[z] == sign:
-					return name
-		# проверка на ничью
-		b = True
-		for k in self.board.keys():
-			if self.board[k] == k:
-				b = False
-		if b:
-			return 'IS MISSING (Победитель отсутствует)'
+    def validate_input(self, num):
+        """Проверка ввода координаты
+            Вывод: True - если знач. валидно"""
+        if not num.isdigit():
+            print('Вы ввели не число')
+            return False
+        if not 1 <= int(num) <= self.count ** 2:
+            print('Число превысила допустимые пределы')
+            return False
+        if not isinstance(self.board[int(num)], int):
+            print('Это поле уже занято')
+            return False
+        return True
 
-		return None
+    def check_winner(self):
+        """Проверка на присутствие победителя или ничьи"""
+        # проверка выигрывающих комбинаций
+        for name, sign in self.players.values():
+            for arg1, arg2, arg3 in combinations:
+                if self.board[arg1] == self.board[arg2] == self.board[arg3] == sign:
+                    return name
+
+        # проверка на ничью
+        flag = True
+        for k in self.board.keys():
+            if self.board[k] == k:
+                flag = False
+        if flag:
+            return 'IS MISSING (Победитель отсутствует)'
+
+        return None
 
 
 if __name__ == '__main__':
-	game = TicTacGame()
-	game.start_game()
+    game = TicTacGame()
+    game.start_game()
---
+++ tests.py
@@ -0,0 +1,47 @@
+"""
+    Тестирование
+"""
+
+import unittest
+from unittest.mock import patch
+from main import TicTacGame
+
+
+class TestTicTacGame(unittest.TestCase):
+    """Тесты игры в крестики-нолики"""
+    def setUp(self):
+        self.game = TicTacGame()
+
+    def test_validate_input(self):
+        """Тест правильного ввода имени игрока"""
+        for i in range(1, self.game.count + 1):
+            self.assertTrue(self.game.validate_input(str(i)))
+
+    @patch('builtins.print')
+    def test_invalidate_input(self, _):
+        """Тест неправильного ввода имени игрока"""
+        for i in [-1, 0, self.game.count ** 2 + 1, self.game.count ** 2 * 2]:
+            self.assertFalse(self.game.validate_input(str(i)))
+
+    def test_check_winner(self):
+        """Тест на проверку чьей то победы"""
+        test_combinations = []
+        self._make_test_combinations(test_combinations)
+        for text_comb, win in test_combinations:
+            self.game.board = text_comb
+            self.assertEqual(self.game.check_winner(), win)
+
+    @classmethod
+    def _make_test_combinations(cls, test):
+        test.append(({i: i for i in range(1, 10)}, None))
+        test.append(({i: i if not (i in (1, 2, 3)) else 'o' for i in range(1, 10)}, 'player1'))
+        test.append(({i: i if not (i in (2, 5, 8)) else 'o' for i in range(1, 10)}, 'player1'))
+        test.append(({i: i if not (i in (1, 5, 9)) else '+' for i in range(1, 10)}, 'player2'))
+        test.append(({i: i if not (i in (3, 5, 7)) else '+' for i in range(1, 10)}, 'player2'))
+        test.append(({i: i if not (i in (3, 6, 9)) else 'o' for i in range(1, 10)}, 'player1'))
+        test.append(({1: 'o', 2: '+', 3: '+', 4: 'x', 5: 'o', 6: 'o', 7: 'o', 8: '+', 9: '+'},
+                     'IS MISSING (Победитель отсутствует)'))
+
+
+if __name__ == '__main__':
+    unittest.main()
