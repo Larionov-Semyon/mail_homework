9a6bb206c24e2bb0bb3e9e92622dc5117a15c509
Larionov-Semyon <37975787+Larionov-Semyon@users.noreply.github.com>
2021-10-23 00:06:20 +0300

    22.10.21 - check num 1 (7/9)

--- homework_2 (Объекты)/metaclass.py
+++ homework_2 (Объекты)/metaclass.py
@@ -1,16 +1,11 @@
 
 class CustomMeta(type):
     """Метакласс: в начале названий всех атрибутов и методов (кроме магических) добавляет префикс "custom_"""
-    def __call__(self, *args, **kwargs):
-        attr = {}
-        for name, val in dct.items():
-            if not name.startswith('__'):
-                attr[f'custom_{name}'] = val
-            else:
-                attr[name] = val
-
-        print(attr, dct)
-        return super().__call__(cls, clsname, bases, attr)
+    def __call__(cls, *args, **kwargs):
+        print(dir(cls))
+        call = type.__call__(cls, *args)
+        setattr(cls, "hello", cls.x)
+        return cls
         # return super(CustomMeta, cls).__init__(cls, name, bases, attr)
 
     # def __new__(cls, clsname, bases, dct):
@@ -51,7 +46,7 @@ class CustomClass(metaclass=CustomMeta):
 if __name__ == '__main__':
     inst = CustomClass()
     print(dir(inst))
-    print(inst.custom_x)
+    # print(inst.custom_x)
     # print(inst.custom_val)
     # print(inst.custom_line())
     # print(inst.val)
